8af2845b25b0b458e76ec6691f805c32bdd74c68
Edwin Antonio Sion Tepe <tepe1600@gmail.com>
2024-09-06 00:58:53 -0600

    se configuro app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,45 +1,28 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TasksModule } from './tasks/tasks.module';
+import { Task } from './tasks/task.entity';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
+    ConfigModule.forRoot(),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => {
-        const useExternalUrl = configService.get('DATABASE_URL');
-        if (useExternalUrl) {
-          return {
-            type: 'postgres',
-            url: useExternalUrl,
-            entities: [__dirname + '/**/*.entity{.ts,.js}'],
-            synchronize: false, // Set to true for development, false for production
-            ssl: {
-              rejectUnauthorized: false
-            }
-          };
-        } else {
-          return {
-            type: 'postgres',
-            host: configService.get('DB_HOSTNAME'),
-            port: configService.get('DB_PORT'),
-            username: configService.get('DB_USERNAME'),
-            password: configService.get('DB_PASSWORD'),
-            database: configService.get('DB_NAME'),
-            entities: [__dirname + '/**/*.entity{.ts,.js}'],
-            synchronize: false, // Set to true for development, false for production
-            ssl: {
-              rejectUnauthorized: false
-            }
-          };
-        }
-      },
+      useFactory: (configService: ConfigService) => ({
+        
+        type: 'postgres',
+        host: configService.get('DB_HOSTNAME'),
+        port: +configService.get<number>('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        entities: ["dist/**/*.entity{.ts,.js}"],
+        synchronize: false, // Siempre false en producción
+      }),
       inject: [ConfigService],
     }),
-    // ... otros módulos
+    TasksModule,
   ],
 })
 export class AppModule {}
